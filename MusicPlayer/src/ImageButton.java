ed37f3a7635b4eef75feedb323498b7d7e6355e8
x40arcade <sobrepenakenneth97@gmail.com>
2025-05-01 18:16:30 +0800

    Commit 4
    added some ui elements but still on the work, also did some animation for the sprites i might changed them later because art takes time, pixel art by
    done by my friend thank you so much, i will do my best to make the ui gud.

--- MusicPlayer/src/ImageButton.java
+++ MusicPlayer/src/ImageButton.java
@@ -1,14 +1,20 @@
+
 import java.awt.event.*;
 import javax.swing.*;
 
 public class ImageButton extends JButton {
+
     private boolean isClicked = false;
     private final MusicPlayer player = new MusicPlayer(); // music player instance
 
-    private final ImageIcon defaultIcon = new ImageIcon("src/assets/default.png");
-    private final ImageIcon transition1 = new ImageIcon("src/assets/transition1.png");
-    private final ImageIcon transition2 = new ImageIcon("src/assets/transition2.png");
-    private final ImageIcon clickedIcon = new ImageIcon("src/assets/clicked.png");
+    //Animation stuff
+    private final ImageIcon defaultIcon = new ImageIcon("src/assets/button1.png");
+    private final ImageIcon transition1 = new ImageIcon("src/assets/button2.png");
+    private final ImageIcon transition2 = new ImageIcon("src/assets/button3.png");
+    private final ImageIcon transition3 = new ImageIcon("src/assets/button4.png");
+    private final ImageIcon transition4 = new ImageIcon("src/assets/button5.png");
+    private final ImageIcon transition5 = new ImageIcon("src/assets/button6.png");
+    private final ImageIcon clickedIcon = new ImageIcon("src/assets/button7.png");
 
     public ImageButton() {
         setIcon(defaultIcon);
@@ -19,7 +25,7 @@ public class ImageButton extends JButton {
         setBounds(0, 0, defaultIcon.getIconWidth(), defaultIcon.getIconHeight());
 
         addActionListener(e -> {
-            Timer transitionTimer = new Timer(100, null);
+            Timer transitionTimer = new Timer(30, null);
             final int[] frame = {0};
 
             transitionTimer.addActionListener(new ActionListener() {
@@ -29,21 +35,37 @@ public class ImageButton extends JButton {
                     if (!isClicked) {
                         // Play forward transition
                         switch (frame[0]) {
-                            case 1 -> setIcon(transition1);
-                            case 2 -> setIcon(transition2);
-                            case 3 -> {
+                            case 1 ->
+                                setIcon(transition1);
+                            case 2 ->
+                                setIcon(transition2);
+                            case 3 ->
+                                setIcon(transition3);
+                            case 4 ->
+                                setIcon(transition4);
+                            case 5 ->
+                                setIcon(transition5);
+                            case 6 -> {
                                 setIcon(clickedIcon);
                                 isClicked = true;
-                                player.play("src/assets/music.wav"); // <-- play music
+                                player.play("src/assets/music3.wav"); // <-- play music
                                 ((Timer) evt.getSource()).stop();
                             }
                         }
                     } else {
                         // Play reverse transition
                         switch (frame[0]) {
-                            case 1 -> setIcon(transition2);
-                            case 2 -> setIcon(transition1);
-                            case 3 -> {
+                            case 1 ->
+                                setIcon(transition5);
+                            case 2 ->
+                                setIcon(transition4);
+                            case 3 ->
+                                setIcon(transition3);
+                            case 4 ->
+                                setIcon(transition2);
+                            case 5 ->
+                                setIcon(transition1);
+                            case 6 -> {
                                 setIcon(defaultIcon);
                                 isClicked = false;
                                 player.stop(); // <-- stop music
--- MusicPlayer/src/Main.java
+++ MusicPlayer/src/Main.java
@@ -1,107 +1,132 @@
+
 import java.awt.Color;
+import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLayeredPane;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
-public class Main extends JFrame{
+public class Main extends JFrame {
 
-	Main(){
-		//Border test
-		//Border border = BorderFactory.createLineBorder(Color.black, 3);
+    Main() {
+        //Border test
+        //Border border = BorderFactory.createLineBorder(Color.black, 3);
 
-		//Make an art for redPanel 980x250
-		// hope this works animation test
+        //Make an art for redPanel 980x250
+        // hope this works animation test
+        // Green Panel button
+        ImageButton animatedButton = new ImageButton();
 
-		// Image Button test
-		JButton imagePlayButton = new JButton();
-		ImageIcon btnPlay = new ImageIcon("MUSICPLAYER/src/assets/btnPlay.png");
-		imagePlayButton.setIcon(btnPlay);
-		imagePlayButton.setBorder(null);
-		imagePlayButton.setContentAreaFilled(false);
-		
-		//Menu Bars
-		JMenuBar menuBar = new JMenuBar();
-		
-		//Menu in the top
-		JMenu filemenu = new JMenu("File");
-		JMenu editmenu = new JMenu("Edit");
-		JMenu helpmenu = new JMenu("Help");
-		
-		menuBar.add(filemenu);
-		menuBar.add(editmenu);
-		menuBar.add(helpmenu);
-		
-		//Menu items
-		JMenuItem loadItem = new JMenuItem("Import");
-		JMenuItem saveItem = new JMenuItem("save");
-		JMenuItem exitItem = new JMenuItem("exit");
-		
-		filemenu.add(loadItem);
-		filemenu.add(saveItem);
-		filemenu.add(exitItem);
-		
-		this.setJMenuBar(menuBar);
-		this.setVisible(true);
-		
-		//Panels colored panel you see in the screen
-		JPanel greenPanel = new JPanel();
-		greenPanel.setBackground(Color.GREEN);
-		greenPanel.setBounds(0, 10, 200, 100);
+        // Acosta pixel art
+        ImageIcon acostaArt1 = new ImageIcon("src/assets/art1.jpg");
+        Image scaled2 = acostaArt1.getImage().getScaledInstance(990, 250, Image.SCALE_SMOOTH);
+        JLabel imageLabel2 = new JLabel(new ImageIcon(scaled2));
+        imageLabel2.setBounds(0, 0, 990, 250);
 
+        //Image for the blue panel
+        ImageIcon table = new ImageIcon("src/assets/table.png");
+        Image scaled = table.getImage().getScaledInstance(980, 100, Image.SCALE_SMOOTH);
+        JLabel imageLabel1 = new JLabel(new ImageIcon(scaled));
+        imageLabel1.setBounds(0, 0, 980, 100);
 
-		JPanel redPanel = new JPanel();
-		redPanel.setBackground(Color.RED);
-		redPanel.setBounds(0, 200, 980, 250);
-		redPanel.setOpaque(true);
-		redPanel.setLayout(null);
-		
-		JPanel bluePanel = new JPanel();
-		bluePanel.setBackground(Color.BLUE);
-		bluePanel.setBounds(0, 10, 980, 100);
-		
-		JPanel yellowPanel = new JPanel();
-		yellowPanel.setBackground(Color.YELLOW);
-		yellowPanel.setBounds(0, 0, 100, 50);
-		
-		//LayeredPane lmao this is not used yet
-		JLayeredPane layeredPane = new JLayeredPane();
-		layeredPane.add(redPanel, Integer.valueOf(0));
-		layeredPane.add(bluePanel, Integer.valueOf(1));
-		layeredPane.add(yellowPanel, Integer.valueOf(2));
-		
-		// JFrame for the screen
-		
-		this.setBackground(Color.BLACK); //Method
-		this.setSize(980, 550);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// method to exit program when window closes
-        this.setVisible(true);   
-        this.setResizable(true);
-		//redPanel.add(animationPanel);
-		//bluePanel.add(imagePlayButton);
-		// Create and add custom image button to greenPanel
-		ImageButton animatedButton = new ImageButton();
-		greenPanel.setLayout(null); // important for setBounds to work
-		greenPanel.add(animatedButton);
-		this.add(greenPanel);
-		this.add(redPanel);
+        // Image Button test
+        JButton imagePlayButton = new JButton();
+        ImageIcon btnPlay = new ImageIcon("src/assets/button.png");
+        imagePlayButton.setIcon(btnPlay);
+        imagePlayButton.setBorder(null);
+        imagePlayButton.setContentAreaFilled(false);
+
+        //Menu Bars
+        JMenuBar menuBar = new JMenuBar(); //Constructor
+
+        //Menu in the top
+        JMenu filemenu = new JMenu("File"); //Methods
+        JMenu editmenu = new JMenu("Edit");
+        JMenu helpmenu = new JMenu("Help");
+
+        menuBar.add(filemenu);
+        menuBar.add(editmenu);
+        menuBar.add(helpmenu);
+
+        //Menu items
+        JMenuItem loadItem = new JMenuItem("Import");
+        JMenuItem saveItem = new JMenuItem("save");
+        JMenuItem exitItem = new JMenuItem("exit");
+
+        filemenu.add(loadItem);
+        filemenu.add(saveItem);
+        filemenu.add(exitItem);
+
+        this.setJMenuBar(menuBar);
+        this.setVisible(true);
+
+        //Panels colored panel you see in the screen
+        JPanel buttonPlay = new JPanel();
+        buttonPlay.setBackground(Color.GREEN);
+        buttonPlay.setBounds(40, 150, 129, 60);
+
+        JPanel buttonPause = new JPanel();
+        buttonPause.setBackground(Color.pink);
+        buttonPause.setBounds(200, 150, 129, 60);
+
+        JPanel buttonStop = new JPanel();
+        buttonStop.setBackground(Color.CYAN);
+        buttonStop.setBounds(360, 150, 129, 60);
+
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.RED);
+        redPanel.setBounds(0, 240, 980, 250);
+        redPanel.setOpaque(true);
+        redPanel.setLayout(null);
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.BLUE);
+        bluePanel.setBounds(0, 140, 980, 100);
+        bluePanel.setLayout(null);
+        bluePanel.setOpaque(false);
+
+        JPanel yellowPanel = new JPanel();
+        yellowPanel.setBackground(Color.YELLOW);
+        yellowPanel.setBounds(0, 0, 1000, 140);
+
+        // JFrame for the screen
+        // The green panel (button) is sized 129x60
+        this.setBackground(Color.BLACK); //Method
+        this.setTitle("MusicPlayer by: x40Arcade");
+        this.setSize(995, 550);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// method to exit program when window closes 
+        this.setLayout(null);
+        this.setResizable(false);
+        // Add the play button to the green panel
+        buttonPlay.setLayout(null); // important for setBounds to work
+        imagePlayButton.setBounds(0, 0, 129, 60); // Set bounds for the button
+
+        // PANEL STUFF
+        this.add(buttonPause);
+        buttonPlay.add(animatedButton);
+        bluePanel.add(imageLabel1);
+        redPanel.add(imageLabel2);
+        this.add(buttonStop);
+        this.add(buttonPlay);
         this.add(bluePanel);
-		this.add(yellowPanel);
-		
-		//this.getRootPane().setBorder(BorderFactory.createLineBorder(Color.black, 3));
+        this.add(redPanel);
+        this.add(yellowPanel);
+        this.setVisible(true);
+        //this.getRootPane().setBorder(BorderFactory.createLineBorder(Color.black, 3));
 
         ImageIcon icon = new ImageIcon("src/assets/icon.jpg"); // Constructor for an image icon
-        this.setIconImage(icon.getImage());      
-		
-		// Action listener to check if the button is pressed
-		imagePlayButton.addActionListener(e -> System.err.println("Image Button pressed"));
-	}	
-	public static void main(String[] args) {
-		Main mainFrame = new Main();
-		mainFrame.setVisible(true);
-	}
+        this.setIconImage(icon.getImage());
+
+        // Action listener to check if the button is pressed
+        imagePlayButton.addActionListener(e -> System.err.println("Image Button pressed"));
+    }
+
+    public static void main(String[] args) {
+        Main mainFrame = new Main();
+        mainFrame.setVisible(true);
+    }
 }
